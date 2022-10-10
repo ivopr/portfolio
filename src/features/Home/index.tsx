5069fa820948f6daed79d779af457be97e9a1de5
Ivo <ivopr@proton.me>
2022-10-10 14:34:33 -0400

    feat: add cv link

--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -1,8 +1,11 @@
-import { FC, lazy } from "react";
+import { FC } from "react";
 import { useTranslation } from "react-i18next";
 import { changePageTitle } from "../../utils/changePageTitle";
 import { badgeData } from "./badgeData";
 import me from "../../assets/me.jpeg";
+import { Button } from "../../layouts/components/Button";
+import { BiLinkExternal } from "react-icons/bi";
+
 
 const Home: FC = () => {
   const { t } = useTranslation(["common", "home", "navigation"]);
@@ -10,9 +13,9 @@ const Home: FC = () => {
   changePageTitle(t("navigation:about-me"), t("common:app-name"));
 
   return (
-    <>
+    <main className="mx-auto md:w-4/5 lg:w-4/6">
       {/* Introduction */}
-      <section className="mx-auto flex flex-col items-center gap-6 md:flex-row md:w-4/5 lg:w-4/6">
+      <section className="flex flex-col items-center gap-6 md:flex-row">
         <div className="m-auto w-52 h-52 rounded-full shadow-md shadow-primary-500 overflow-hidden">
           <img 
             className="w-full h-full"
@@ -36,9 +39,18 @@ const Home: FC = () => {
               />
             ))}
           </div>
+          <h5 className="mt-5 text-sm">{t("home:cv.title")}</h5>
+          <div className="flex flex-wrap gap-2 mt-2">
+            <a href="http://lattes.cnpq.br/5130583751808996" className="flex flex-1">
+              <Button className="flex justify-center items-center gap-2">
+                {t("home:cv.lattes")}
+                <BiLinkExternal className="h-4 w-4" />
+              </Button>
+            </a>
+          </div>
         </div>
       </section>
-    </>
+    </main>
   )
 }
 
--- src/layouts/components/Button/index.tsx
+++ src/layouts/components/Button/index.tsx
@@ -3,11 +3,12 @@ import { FC, ReactNode } from "react";
 type ButtonProps = {
   children: ReactNode;
   className?: string;
+  parentClassName?: string;
 }
 
-export const Button: FC<ButtonProps> = ({ children, className }) => {
+export const Button: FC<ButtonProps> = ({ children, className, parentClassName }) => {
   return (
-    <button className="relative inline-block text-sm font-medium text-primary-500 group active:text-primary-500 focus:outline-none focus:ring">
+    <button className={`relative inline-block text-sm font-medium text-primary-500 group active:text-primary-500 focus:outline-none focus:ring ${parentClassName}`}>
       <span
         className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-primary-500 group-hover:translate-y-0 group-hover:translate-x-0"
       />
