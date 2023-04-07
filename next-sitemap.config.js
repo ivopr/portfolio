c0c9ef45582015129f31963eca314e3c49c1464f
Ivo <ivo@vist.gg>
2023-04-07 11:16:58 -0400

    fix: sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://localhost:3000",
+  siteUrl: process.env.VERCEL_URL || "https://localhost:3000",
   generateRobotsTxt: true // (optional)
   // ...other options
 };
